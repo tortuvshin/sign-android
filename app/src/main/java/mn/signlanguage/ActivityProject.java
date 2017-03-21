af783f70da82922b8da7cf00f9d4563366b136a9
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-03-21 14:35:12 +0800

    updated project about

--- app/src/main/java/mn/signlanguage/ActivityProject.java
+++ app/src/main/java/mn/signlanguage/ActivityProject.java
@@ -12,11 +12,11 @@ public class ActivityProject extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_activity_project);
+        setContentView(R.layout.activity_project);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Төслийн тухай");
         webView = (WebView)findViewById(R.id.pWebView);
-        String a = "<!DOCTYPE html>\n" +
+        String mn = "<!DOCTYPE html>\n" +
                 "<html>\n" +
                 "<body>\n" +
                 "\n" +
@@ -49,7 +49,41 @@ public class ActivityProject extends AppCompatActivity {
                 "\n" +
                 "</body>\n" +
                 "</html>\n";
-        webView.loadData(a, "text/html; charset=utf-8", "utf-8");
+
+        String a = "<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<body>\n" +
+                "\n" +
+                "<p>Founded by US. Embassy Grants Program\n</p>\n" +
+                "\n" +
+                "<p>Project name: Bridging the Gap Darkhan Sign - Language Communication project </p>\n" +
+                "<p>Team members:</p>\n" +
+                "<p style=\"margin-left: 20px\">\n" +
+                "            1. Janar Mukhtar (team leader)<br>\n" +
+                "            2. Bolor Battulga<br>\n" +
+                "            3. Solongo Dorjpalam<br>\n" +
+                "            4. Uranzaya Bayarsaikhan<br>\n" +
+                "            5. Bolorchimeg Batmunkh<br>\n" +
+                "            6. Oyu-Erdene Batsaikhan<br>\n" +
+                "            7. Naranmandakh Ankhbayar<br>\n" +
+                "            8. Anar Ganzorig<br>\n" +
+                "            9. Namuun Atarzol<br>\n" +
+                "            10. Amar-Erdene Tsogtsaikhan<br>\n" +
+                "            </p>\n" +
+                "<p>Coordinated by::</p>\n" +
+                "<p style=\"margin-left:20px\">\n" +
+                "            Morgan Thomas<br>\n" +
+                "            Chuluuntumur Damdin<br></p>\n" +
+                "\n" +
+                "<p> Developed by:</p>\n" +
+                "<p style=\"margin-left:20px\">\n" +
+                "            Ts.Naranmandakh<br>\n" +
+                "            B.Turtuvshin <br></p>\n" +
+                "\n" +
+                "\n" +
+                "</body>\n" +
+                "</html>\n";
+        webView.loadData(mn, "text/html; charset=utf-8", "utf-8");
     }
 
     @Override
